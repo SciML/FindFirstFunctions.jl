ee000f3affad66ecc026240da6cda9057dee4dcc
Chris Elrod <elrodc@gmail.com>
2024-01-23 18:45:56 -0500

    findfirstequal for `DenseVector{Int}` only instead of generic because YAGNI (probably)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,21 @@ using FindFirstFunctions
 using Test
 
 @testset "FindFirstFunctions.jl" begin
-    # Write your tests here.
+
+  for n = 0:128
+    x = unique!(rand(Int, n))
+    for i = eachindex(x)
+      @test FindFirstFunctions.findfirstequal(x[i], x) == i
+    end
+    if length(x) > 0
+      @test FindFirstFunctions.findfirstequal(x[begin], @view(x[begin:end])) === 1
+      @test FindFirstFunctions.findfirstequal(x[begin], @view(x[begin+1:end])) === nothing
+      @test FindFirstFunctions.findfirstequal(x[end], @view(x[begin:end-1])) === nothing
+    end
+    y = rand(Int)
+    @test FindFirstFunctions.findfirstequal(y, x) === findfirst(==(y), x)
+  end
+
 end
+
+
