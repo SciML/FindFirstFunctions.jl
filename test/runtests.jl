8bb2b8c0529394c272bd606dbef3798237c29933
Chris Elrod <elrodc@gmail.com>
2024-01-25 16:24:58 -0500

    add sorted method

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,11 @@ using Test
 
   for n = 0:128
     x = unique!(rand(Int, n))
+    s = sort(x)
     for i = eachindex(x)
       @test FindFirstFunctions.findfirstequal(x[i], x) == i
+      @test FindFirstFunctions.findfirstequal(s[i], s) == i
+      @test FindFirstFunctions.findfirstsortedequal(s[i], s) == i
     end
     if length(x) > 0
       @test FindFirstFunctions.findfirstequal(x[begin], @view(x[begin:end])) === 1
@@ -14,7 +17,9 @@ using Test
       @test FindFirstFunctions.findfirstequal(x[end], @view(x[begin:end-1])) === nothing
     end
     y = rand(Int)
-    @test FindFirstFunctions.findfirstequal(y, x) === findfirst(==(y), x)
+    ff = findfirst(==(y), x)
+    @test FindFirstFunctions.findfirstequal(y, x) === ff
+    ff === nothing && @test FindFirstFunctions.findfirstsortedequal(y, x) === nothing
   end
 
 end
